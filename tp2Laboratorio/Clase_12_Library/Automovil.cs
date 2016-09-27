e72c16c8e9f21bc1c135315ba62b52769df789a8
AgustinPrado <agusprado92@gmail.com>
2016-09-26 22:21:26 -0300

    20160926 - v1.0
    Bugs: el método Mostrar no es independiente en cada clase. Polimorfismo
    que todavía no vimos.

---
+++ tp2Laboratorio/Clase_12_Library/Automovil.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase_12_Library_2;
+
+namespace Clase_12_Library
+{
+    public class Automovil : Vehiculo
+    {
+        public Automovil(EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+        }
+        /// <summary>
+        /// Los automoviles tienen 4 ruedas
+        /// </summary>
+        public override short CantidadRuedas
+        {
+            get
+            {
+                return 4;
+            }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("AUTOMOVIL");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("RUEDAS : {0}", this.CantidadRuedas);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ tp2Laboratorio/Clase_12_Library/Camion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase_12_Library_2;
+
+namespace Clase_12_Library
+{
+    public class Camion : Vehiculo
+    {
+        public Camion(EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+        }
+        /// <summary>
+        /// Los camiones tienen 8 ruedas
+        /// </summary>
+        public override short CantidadRuedas
+        {
+            get
+            {
+                return 8;
+            }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CAMION");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("RUEDAS : {0}", this.CantidadRuedas);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
