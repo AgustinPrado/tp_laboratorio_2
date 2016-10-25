e0542d498da1890a76f4917ff2b3d51531c3f936
unknown <agusprado92@gmail.com>
2016-10-24 21:37:17 -0300

    Comienzo del TP

---
+++ tp3Laboratorio/Prado.Agustin.2D.TP3/EntidadesAbstractas/Persona.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesAbstractas
+{
+    public abstract class Persona
+    {
+        public enum ENacionalidad
+        {
+            Argentino,
+            Extranjero
+        }
+
+        private string _apellido;
+        public string Apellido
+        {
+            set
+            {
+                // VALIDAR
+                this._apellido = value;
+            }
+            get
+            {
+                return this._apellido;
+            }
+        }
+        private string _nombre;
+        public string Nombre
+        {
+            set
+            {
+                // VALIDAR
+                this._nombre = value;
+            }
+            get
+            {
+                return this._nombre;
+            }
+        }
+        private int _dni;
+        
+        private ENacionalidad _nacionalidad;
+    }
+}
