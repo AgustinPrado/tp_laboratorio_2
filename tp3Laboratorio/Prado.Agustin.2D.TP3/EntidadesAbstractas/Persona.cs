ab191e4c3341aca7b5d9081a89e825f1a57685b9
unknown <agusprado92@gmail.com>
2016-10-25 21:24:42 -0300

    Comienzo a hacer la clase Persona. Probablemente se cambien las consignas del TP.

--- tp3Laboratorio/Prado.Agustin.2D.TP3/EntidadesAbstractas/Persona.cs
+++ tp3Laboratorio/Prado.Agustin.2D.TP3/EntidadesAbstractas/Persona.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Excepciones;
 
 namespace EntidadesAbstractas
 {
@@ -14,6 +15,7 @@ namespace EntidadesAbstractas
             Extranjero
         }
 
+        #region ATRIBUTOS Y PROPIEDADES
         private string _apellido;
         public string Apellido
         {
@@ -41,7 +43,86 @@ namespace EntidadesAbstractas
             }
         }
         private int _dni;
-        
+        public int DNI
+        {
+            set
+            {
+                this._dni = this.ValidarDni(this.Nacionalidad, value);
+            }
+            get
+            {
+                return this._dni;
+            }
+        }
         private ENacionalidad _nacionalidad;
+        public ENacionalidad Nacionalidad
+        {
+            set
+            {
+                // VALIDAR
+                this._nacionalidad = value;
+            }
+            get
+            {
+                return this._nacionalidad;
+            }
+        }
+        public string StringToDNI
+        {
+            set
+            {
+                // VALIDAR
+                this.DNI = this.ValidarDni(this.Nacionalidad, value);
+            }
+        }
+        #endregion
+
+        #region CONSTRUCTORES
+        public Persona(string nombre, string apellido, ENacionalidad nacionalidad)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Nacionalidad = nacionalidad;
+        }
+
+        public Persona(string nombre, string apellido, int dni, ENacionalidad nacionalidad)
+            : this(nombre, apellido, nacionalidad)
+        {
+            this.DNI = dni;
+        }
+
+        public Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+            : this(nombre, apellido, nacionalidad)
+        {
+            this.StringToDNI = dni;
+        }
+        #endregion
+
+        #region MÉTODOS
+        private int ValidarDni(ENacionalidad nacionalidad, int dato)
+        {
+            if (nacionalidad == ENacionalidad.Argentino)
+            {
+                if ((dato >= 1) && (dato <= 89999999))
+                {
+                    return dato;
+                }
+            }
+            else
+            {
+                if ((dato >= 90000000) && (dato <= 99999999))
+                {
+                    return dato;
+                }
+            }
+            // VA A CAMBIAR LA EXCEPCION
+            throw new NacionalidadInvalidaException("La nacionalidad no se condice con el número de DNI");
+        }
+        private int ValidarDni(ENacionalidad nacionalidad, string dato)
+        {
+            // VALIDAR
+            return int.Parse(dato);
+        }
+        #endregion
     }
 }
